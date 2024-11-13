d19b831e5ce99981039c15239418e22e6ae0edf4
https://github.com/Devisrar <israr.exs@gmail.com>
2024-11-13 18:03:07 +0500

    feature/reset/verify password

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { MailerModule } from '@nestjs-modules/mailer';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { join } from 'path';
 
 @Module({
   imports: [
-    // Load .env configuration globally
     ConfigModule.forRoot({ isGlobal: true }),
-
-    // Database configuration using environment variables
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -24,31 +20,29 @@ import { join } from 'path';
         password: configService.get<string>('DATABASE_PASSWORD'),
         database: configService.get<string>('DATABASE_NAME'),
         autoLoadEntities: true,
-        synchronize: true, // Disable in production
+        synchronize: true,
       }),
     }),
 
-    // Mailer configuration using environment variables
     MailerModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         transport: {
-          host: configService.get<string>('EMAIL_HOST'), // e.g., smtp.gmail.com
-          port: configService.get<number>('EMAIL_PORT'), // e.g., 587
-          secure: false, // true for 465, false for other ports
+          host: configService.get<string>('EMAIL_HOST'), 
+          port: configService.get<number>('EMAIL_PORT'), 
+          secure: false, 
           auth: {
-            user: configService.get<string>('EMAIL_USER'), // Email address
-            pass: configService.get<string>('EMAIL_PASS'), // App password
+            user: configService.get<string>('EMAIL_USER'), 
+            pass: configService.get<string>('EMAIL_PASS'), 
           },
         },
         defaults: {
-          from: configService.get<string>('EMAIL_SENDER'), // Default sender email
+          from: configService.get<string>('EMAIL_SENDER'), 
         },
         template: {
           // Ensure templates are correctly located regardless of the build environment
-          dir: join(process.cwd(), 'src', 'templates'), // Use absolute path to src/templates
-          adapter: new HandlebarsAdapter(),
+          dir: join(process.cwd(), 'src', 'templates'), 
           options: {
             strict: true,
           },
@@ -56,7 +50,6 @@ import { join } from 'path';
       }),
     }),
 
-    // Import other application modules
     UsersModule,
     AuthModule,
   ],
